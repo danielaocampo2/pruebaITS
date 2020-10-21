c8448aeb89a4caa0deb946601caa53212485e84f
Daniela_Ocampo <danielaocampoo@gmail.com>
2020-10-21 17:17:20 -0500

    Ensayo

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "nombre:  ";
+print_r($_POST["lis_person_name_full"]);
+echo "  correo:   ";
+print_r($_POST["lis_person_contact_email_primary"]);
+echo "  id usuario:   ";
+print_r($_POST["user_id"]);
+//echo "***************************************GET---->";
+//print_r($_GET);
+
+//print_r($_REQUEST);
+?>
